09b344133e3833e292c07f753d663d03dbc79785
Pavel Alimpiev <7024354+VaultVulp@users.noreply.github.com>
2020-06-09 12:50:08 +0300

    Reorder commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,11 @@ fi
 DOCKER_IMAGE_NAME=$(echo docker.pkg.github.com/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME} | tr '[:upper:]' '[:lower:]')
 DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} | tr '[:upper:]' '[:lower:]')
 
+docker login -u publisher -p ${DOCKER_TOKEN} docker.pkg.github.com
+
 if [ $PULL_IMAGE == "true" ]; then
   docker pull $DOCKER_IMAGE_NAME_WITH_TAG || docker pull $DOCKER_IMAGE_NAME || 1
 fi
 
-docker login -u publisher -p ${DOCKER_TOKEN} docker.pkg.github.com
 docker build -t $DOCKER_IMAGE_NAME_WITH_TAG -f $DOCKERFILE $BUILD_CONTEXT
 docker push $DOCKER_IMAGE_NAME_WITH_TAG
