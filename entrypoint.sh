e246a9e09fbf6acdbff62b41bf13fb68a86f91af
Pavel Alimpiev <7024354+VaultVulp@users.noreply.github.com>
2023-01-13 15:53:38 +0300

    Fix build context handling (#22)

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,17 @@ if [ $PULL_IMAGE == "true" ]; then
   docker pull $DOCKER_IMAGE_NAME_WITH_TAG || docker pull $DOCKER_IMAGE_NAME || 1
 fi
 
-set -- -t $DOCKER_IMAGE_NAME_WITH_TAG -f $DOCKERFILE $CUSTOM_DOCKER_BUILD_ARGS $BUILD_CONTEXT
+set -- -t $DOCKER_IMAGE_NAME_WITH_TAG
+
+if [ $DOCKERFILE != "Dockerfile" ]; then
+  set -- "$@" -f $DOCKERFILE
+fi
+
+if [ $CUSTOM_DOCKER_BUILD_ARGS != "" ]; then
+  set -- "$@"  $CUSTOM_DOCKER_BUILD_ARGS
+fi
+
+set -- "$@" $BUILD_CONTEXT
 
 for tag in $DOCKER_IMAGE_TAGS
 do
